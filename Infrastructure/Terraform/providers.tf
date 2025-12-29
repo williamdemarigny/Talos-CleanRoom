2cd8814474c210b77f6a8311f2d4ee0a90d09eed
Mavlite <williamdemarigny@gmail.com>
2025-12-29 10:36:19 -0600

    updating ignore files

--- Infrastructure/Terraform/providers.tf
+++ Infrastructure/Terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.60.0"
+      version = "0.90.0"
     }
     talos = {
       source = "siderolabs/talos"
-      version = "0.5.0"
+      version = "0.10.0"
     }
   }
 }
